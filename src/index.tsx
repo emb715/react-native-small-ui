19a3864af9e4a1404c6c725bdf43583b71934b83
Ezequiel Benitez <ezequielmartinbenitez@gmail.com>
2024-08-05 20:03:59 -0300

    fix: add export type CustomStyle
    is used in readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
-export { TinyBaseProvider, createComponent, useTinyBase } from './tinybase';
+export {
+  TinyBaseProvider,
+  createComponent,
+  useTinyBase,
+  type CustomStyle,
+} from './tinybase';
 export { useTheme, getTheme } from './hooks/useTheme';
 export * from './utils/helpers';
 export { getHexAlpha, getContrastColor } from './utils/utils.colors';
